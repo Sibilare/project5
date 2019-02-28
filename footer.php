6a837b21e767cba91ad0f03432383f1042cbd637
Stewart Point <photobystewart@gmail.com>
2019-02-28 09:44:10 -0800

    test commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?>
+
+</div><!-- #content -->
+<button id="show_me_another">Show Me Another</button>
+<div></div>
+<br>
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        <ul class="footermenu">
+            <li> <a href="">About</a></li>
+            <li> <a href="">Archives</a></li>
+            <li> <a href="">Submit a Quote</a></li>
+            <p class="footerbrought">Brought to you by <span class="red"><a href="www.redacademy.com">RED Academy</a></span></p>
+            < </div> <!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html> 
